965d1cfe172bef94d41702230634d4cc6d9bde2d
Arch Patel <archpatel4@gmail.com>
2023-10-02 15:49:36 +0530

    created user table

--- app/models.py
+++ app/models.py
@@ -15,3 +15,13 @@ class Post(Base):
     published = Column(Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),
                         server_default=text('now()'), nullable=False)
+
+
+class User(Base):
+    __tablename__ = "orm_user"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        server_default=text('now()'), nullable=False)
