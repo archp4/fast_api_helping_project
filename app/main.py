342e602e616432d9aec36fcdfed5c925c9d3bf6d
Arch Patel <archpatel4@gmail.com>
2023-10-04 15:18:38 +0530

    Get user by id

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, status, HTTPException, Response, Depends
 from typing import List  # Import for pydantic to Tell response will be list
 
 # ORM Files
-from . import models, formatting
+from . import models, formatting,utils
 from .database import engine, getDB
 from sqlalchemy.orm import Session
 
@@ -93,6 +93,8 @@ def update_post_by_id(id: int, payload: formatting.PostCreate, db: Session = Dep
 
 @app.post("/users", status_code=status.HTTP_201_CREATED, response_model=formatting.UserResponse)
 def create_User(payload: formatting.UserRequest, db: Session = Depends(getDB)):
+    #hashing the password
+    hash_password= 
     # converting payload into dictonary then unzipping data
     new_post = models.User(**payload.model_dump())
     db.add(new_post)
@@ -100,3 +102,12 @@ def create_User(payload: formatting.UserRequest, db: Session = Depends(getDB)):
     db.refresh(new_post)
 
     return new_post
+
+
+@app.get("/users/{id}", response_model=formatting.UserResponse)
+def get_user_by_id(id: int, db: Session = Depends(getDB)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"your requested user by id : {id} is not found")
+    return user
